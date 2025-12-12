248a6a8b4b16a916ade77c27f980022cc19a2a6c
utkarsh raj <utkarshbrb6@gmail.com>
2025-12-13 01:26:00 +0530

    Move frontend into frontend folder

---
+++ frontend/src/pages/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SkillCard from '../components/skills/SkillCard'
+
+export default function Skills(){
+  return (
+    
+     <div className="skill">
+      <SkillCard/>
+     </div>
+  )
+}
