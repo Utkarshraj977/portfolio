0030df6b7378ca47a51f4380a519636fae856ed9
Utkarsh Raj <utkarshbrb6@gmail.com>
2026-02-15 21:29:05 +0530

    Update skill levels for various technologies

--- frontend/src/components/skills/SkillCard.jsx
+++ frontend/src/components/skills/SkillCard.jsx
@@ -5,13 +5,13 @@ import { VscSymbolClass,VscSettingsGear  } from "react-icons/vsc";
 import { FaJava } from "react-icons/fa";
 
 const skills = [
-  { name: "React", icon: <FaReact className="text-cyan-400" size={40} />, level: 90 },
+  { name: "React", icon: <FaReact className="text-cyan-400" size={40} />, level: 80 },
   { name: "Express", icon: <SiNextdotjs className="text-white" size={40} />, level: 85 },
   { name: "BootStrap", icon: <SiTypescript className="text-blue-500" size={40} />, level: 80 },
-  { name: "JavaScript", icon: <SiJavascript className="text-yellow-400" size={40} />, level: 95 },
+  { name: "JavaScript", icon: <SiJavascript className="text-yellow-400" size={40} />, level: 85 },
   { name: "HTML5", icon: <FaHtml5 className="text-orange-500" size={40} />, level: 98 },
-  { name: "CSS3", icon: <FaCss3Alt className="text-blue-600" size={40} />, level: 95 },
-  { name: "Tailwind CSS", icon: <SiTailwindcss className="text-teal-400" size={40} />, level: 92 },
+  { name: "CSS3", icon: <FaCss3Alt className="text-blue-600" size={40} />, level: 90 },
+  { name: "Tailwind CSS", icon: <SiTailwindcss className="text-teal-400" size={40} />, level: 60 },
   { name: "Node.js", icon: <FaNodeJs className="text-green-500" size={40} />, level: 75 },
   { name: "MongoDB", icon: <SiMongodb className="text-green-400" size={40} />, level: 70 },
   { name: "Material UI", icon: <SiRedux className="text-purple-500" size={40} />, level: 80 },
